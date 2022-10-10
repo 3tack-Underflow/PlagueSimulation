3c6a5f1044d2ce5839578078dd78b0b0af7cccc6
Alan Sun <56702016+ACCount-Nine38@users.noreply.github.com>
2022-10-10 02:19:35 -0400

    added broken pipe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const loginAction = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      user: Username, 
+      pass: Password
+    }).then(() => {
+      alert("successful insert");
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>Plague Doctor</h1>
+
+      <div className = "Login">
+        <label>Username</label>
+        <input type = "text" name = "Username" onChange = {(e) => {
+          setUsername(e.target.value);
+        }}/>
+        <label>Password</label>
+        <input type = "text" name = "Password" onChange = {(e) => {
+          setPassword(e.target.value);
+        }}/>
+
+        <button onClick = {loginAction}>Login</button>
+        <button onClick = {loginAction}> Register</button>
+      </div>
+
     </div>
   );
 }
