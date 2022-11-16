b087ce0a8d620022eb4fe588868a269168f7c974
Alan Sun <56702016+ACCount-Nine38@users.noreply.github.com>
2022-11-16 14:54:51 -0500

    simulation changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const db = mysql.createPool({
     host: '23.91.84.211',
     user: 'cs',
     password: 'cs348',
-    database: 'user_schema'
+    database: 'user_schema',
+    multipleStatements: true
 });
 
 app.use(cors());
@@ -43,6 +44,74 @@ app.post('/api/insert', (req, res) => {
         console.log(result);
     });
 });
+
+////////////////////////////////////////////////////////
+// Archive Queries
+////////////////////////////////////////////////////////
+
+app.post('/api/insert', (req, res) => {
+    const username = req.body.user; 
+    const password = req.body.pass;
+    
+    const sqlInsert = "INSERT INTO user (username, password) VALUES (?,?);";
+    db.query(sqlInsert, [username, password], (err, result) => {
+        console.log(result);
+    });
+});
+
+////////////////////////////////////////////////////////
+// Simulation Queries
+////////////////////////////////////////////////////////
+app.post('/api/isolate', (req, res) => {
+    const isolationCost = req.body.cost; 
+    const simID = req.body.simID; 
+    const humanID = req.body.humanID; 
+
+    const sqlInsert = 
+    "START TRANSACTION;" + 
+
+    "UPDATE 'simulation'" + 
+    "SET funds = funds - " + isolationCost + ", environment_isolation_capacity = environment_isolation_capacity - 1" +
+    "WHERE id = " + simID + ";" + 
+    
+    "SELECT @human = num" +
+    "FROM simulation_humans" + 
+    "WHERE num = " + humanID + " AND id = " + simID + " AND" + 
+    "isolated = 0 AND status = 'alive';" + 
+    
+    "CALL `user_schema`.`checkRollback`(@human);" + 
+    
+    "UPDATE simulation_humans" +
+    "SET isolated = 1" + 
+    "AND num = " + humanID + " AND id = " + simID + ";" + 
+    
+    "COMMIT";
+    db.query(sqlInsert, (err, result) => {
+        console.log(result);
+    });
+});
+
+/*
+START TRANSACTION;
+
+UPDATE 'simulation'
+SET funds = funds - (isolation cost),
+    environment_isolation_capacity = environment_isolation_capacity - 1
+WHERE id = (simulation id);
+
+SELECT @human = num
+FROM simulation_humans
+WHERE num = (human num) AND id = (simulation id) AND
+isolated = 0 AND status = 'alive';
+
+CALL `user_schema`.`checkRollback`(@human);
+
+UPDATE simulation_humans
+SET isolated = 1
+AND num = (human num) AND id = (simulation id);
+
+COMMIT;
+*/
 
 app.listen(3001, () => {
     console.log("running on port 3001");
