484e54df7f3a84214188a4b7d6de63af47ae470c
Junya <79063799+junyawang02@users.noreply.github.com>
2022-11-17 23:43:23 -0500

    fix to index

--- sql/c4_createIndex.sql
+++ sql/c4_createIndex.sql
@@ -1,10 +1,10 @@
 -- The queries and output often remain the same for production data. These indexes make the production queries more efficient.
 -- test efficiency by executing query with and without index --
 
-CREATE INDEX statusIndex on simulation_humans(status);
+CREATE INDEX status_index on simulation_humans(status);
 
-CREATE INDEX owner ON simulation_participation(username, is_owner);
+CREATE INDEX username_owner_index ON simulation_participation(username, is_owner);
 
-CREATE INDEX owner ON simulation_participation(id, is_owner);
+CREATE INDEX id_owner_index ON simulation_participation(id, is_owner);
 
-CREATE INDEX owner ON infection(plague_id);
+CREATE INDEX plague_id_index ON infection(plague_id);
