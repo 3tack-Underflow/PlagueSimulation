30b4a4f71b5da1427870f71f5d8d48ef5aaa0766
Henry Zhang <henryzhang757@gmail.com>
2022-11-24 23:34:23 -0500

    fix right padding

--- src/Simulation.js
+++ src/Simulation.js
@@ -525,7 +525,7 @@ function Simulation() {
                                                 zIndex: "1000", 
                                                 width: "400px",
                                                 height: "45.8%",
-                                                right: "16.5%",
+                                                right: "300px",
                                                 bottom: "1%"}}>
                     <div className = "title" style={{margin: "15px 0px 0px 0px"}}>
                         <label>
